52b925a100b1b3a5222243fc5310fed49cdfdabd
jjh1976036 <106015923+jjh1976036@users.noreply.github.com>
2025-07-02 20:18:36 +0900

    Update PurchaseRequest.java

--- mybook/src/main/java/mp/infra/dto/PurchaseRequest.java
+++ mybook/src/main/java/mp/infra/dto/PurchaseRequest.java
@@ -2,6 +2,9 @@ package mp.infra.dto;
 
 import lombok.Data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class PurchaseRequest {
     private String userId;
